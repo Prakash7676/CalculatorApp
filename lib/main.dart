770c412ac778506354e2cae7e94b3d2af438be01
Prakash Shrestha <100356351+Prakash7676@users.noreply.github.com>
2024-08-16 18:17:14 +0545

    add

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:calculator/calculator_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CalculatorView(),
+    );
+  }
+}
